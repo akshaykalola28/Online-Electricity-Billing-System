5795ae12f58f4ee1df838483774421d24a2be2cb
Akshay Kalola <akshay.kalola28@gmail.com>
2019-02-28 00:00:22 +0530

    revert demo branch

--- app/src/main/java/example/akshay/onlinebillingsystem/GetDetailsActivity.java
+++ app/src/main/java/example/akshay/onlinebillingsystem/GetDetailsActivity.java
@@ -186,8 +186,6 @@ public class GetDetailsActivity extends Fragment {
         if (month <= 8) {
             monthString = "0" + monthString;
         }
-
-
         return yearString + monthString;
     }
 
